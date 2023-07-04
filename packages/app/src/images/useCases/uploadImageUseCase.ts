cd12480b3fd519a309b111e270364ba79c577998
Stepan Ryabukhin <47939602+step1989@users.noreply.github.com>
2023-07-04 22:03:22 +0500

    feat(): was implement capture rtsp stream (#1)

--- packages/app/src/images/useCases/uploadImageUseCase.ts
+++ packages/app/src/images/useCases/uploadImageUseCase.ts
@@ -1,16 +1,11 @@
-import childProcess from "child_process";
-import fs from "fs/promises";
-import path from "path";
-
-import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import {Injectable} from "@nestjs/common";
 import * as tf from "@tensorflow/tfjs-node";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 
-import { DetectedObjectDto } from "ᐸDtosᐳ";
+import {DetectedObjectDto} from "ᐸDtosᐳ";
 
 @Injectable()
-export class UploadImageUseCase implements OnModuleInit, OnModuleDestroy {
-  private ffmpegStream!: childProcess.ChildProcessWithoutNullStreams;
+export class UploadImageUseCase {
   private model!: cocoSsd.ObjectDetection;
 
   public async execute(
@@ -19,46 +14,5 @@ export class UploadImageUseCase implements OnModuleInit, OnModuleDestroy {
     const data = tf.node.decodeImage(image.buffer, 3);
 
     return this.model.detect(data);
-  }
-
-  public onModuleDestroy(): void {
-    this.ffmpegStream.kill("SIGKILL");
-  }
-
-  public async onModuleInit(): Promise<void> {
-    this.model = await cocoSsd.load();
-
-    this.ffmpegStream = childProcess.spawn("ffmpeg", [
-      "-i",
-      "rtsp://IP:8554/", // TODO: use you IP here!
-      "-r",
-      "1",
-      "-f",
-      "image2pipe",
-      "-vcodec",
-      "mjpeg",
-      "-q:v",
-      "1",
-      "-",
-    ]);
-
-    let index = 1;
-
-    this.ffmpegStream.stdout.on("data", (data: Buffer) => {
-      fs.writeFile(
-        path.join(process.cwd(), "tmp_stream", "imgs", `${index++}.jpg`),
-        data,
-      ).catch((error) => {
-        console.error(error);
-      });
-    });
-
-    // this.stream.on('exit', (code, signal) => {
-    //   if (code === 1) {
-    //     console.error('RTSP stream exited with error')
-    //     this.exitCode = 1
-    //     return this.emit('exitWithError')
-    //   }
-    // })
   }
 }
